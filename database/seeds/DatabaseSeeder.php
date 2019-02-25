676bad38c09b694fc88bb07f541660d5a1d00978
kaminskij <kaminskiqba@gmail.com>
2019-02-25 20:48:19 +0100

    init commit

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // DB::table('roles')->insert([
+        //     'name' => 'Administrator',
+        //     'email' => Str::random(10).'@gmail.com',
+        //     'password' => bcrypt('secret'),
+        // ]);
+    }
+}
